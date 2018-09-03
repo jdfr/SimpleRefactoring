0b2020c91617be09e28a17871191de945edb331a
josedavid <josedavid@nospam.com>
2018-09-04 01:55:09 +0200

    add the ability to hunt for values being mentioned in header files, as well as the ability to use re-entrant scripts

--- refactor.py
+++ refactor.py
@@ -63,90 +63,130 @@ class ExecuteContext:
 #simple but high-level driver for the refactor binary, it basically does housekeeping and high-level planning; the binary does the grunt work.
 class ExternalRefactor:
     def __init__(self, 
+                 context,
                  translatepath=lambda x: x, 
-                 translatesavepath=lambda x: x, 
                  compiler_args_base=[], 
                  compiler_args=lambda x: ["-I."], 
                  command='./simpleRefactor', 
                  exedir=os.getcwd(), 
                  cppextensions=('.cpp',), 
+                 hppextensions=('.hpp', '.h'),
+                 getCppForHpp=None,
+                 grokscraper=None,
                  isxmlfile=lambda x: x.endswith(('.xml',)), 
                  xml_xpath=None, 
-                 simulate=False):
+                 simulate=False,
+                 verbose=True):
+        self.context = context
         self.translatepath = translatepath
-        self.translatesavepath = translatesavepath
         self.compiler_args_base = compiler_args_base
         self.compiler_args = compiler_args
         self.command = command
         self.exedir = exedir
         self.cppextensions = cppextensions
+        self.hppextensions = hppextensions
+        self.getCppForHpp = getCppForHpp
+        self.grokscraper = grokscraper
         self.xml_xpath = xml_xpath
         self.isxmlfile = isxmlfile
         self.simulate = simulate
-        self.template = lambda term, value, filepath: [self.command, '--term=%s' % term, '--value=%s' % value, filepath, '--']
-    
-    def doCPPFile(self, term, value, filepath, savepath):
-        commandline = self.template(term, value, filepath)+self.compiler_args_base+self.compiler_args(filepath)
-        if self.simulate:
-            print 'ON %s EXECUTE %s' % (self.exedir, ' '.join(commandline))
-        else:
-            if os.path.samefile(filepath, savepath):
-                try:
-                    refactored = subp.check_output(commandline, cwd=self.exedir)
-                    with open(savepath, 'w') as svp:
-                        svp.write(refactored)
-                except CalledProcessError as err:
-                    print err.output
-                    print "REFACTORING TOOL RETURNED ERROR CODE %d" % err.returncode
-            else:
-                subp.call(commandline, cwd=self.exedir, stdout=file(savepath, 'w'))
-    
-    def doXMLFile(self, term, filepath, savepath):
-        if self.simulate:
-            print 'ON %s REMOVE REFERNCES TO %s' % (filepath, term)
-        else:
-            root = etree.parse(filepath)
-            res = root.xpath(self.xml_xpath % term)
-            if len(res)!=1:
-                print "Error locating config value <%s> in XML file <%s>!!!!!" % (term, filepath)
-            else:
-                toremove = res[0]
-                #prev = toremove.getprevious()
-                #if prev is not None and prev.tail is not None:
-                #    toks = prev.tail.rsplit('\n', 1)
-                #    if len(toks)>1:
-                #        prev.tail = toks[0]+'\n'
-                toremove.getparent().remove(toremove)
-                with open(savepath, 'w') as svp:
-                    svp.write(etree.tostring(root))
+        self.verbose = verbose
+        self.template = lambda term, value, filepath: [self.command, '--term=%s' % term, '--value=%s' % value, '--overwrite', filepath, '--']
 
+    #do not forget to call this one if you want to make sure to also refactor instances that only apper in header files!
+    def addCppFilesForHppFiles(self, table):
+        for filename in table:
+            if filename.endswith(self.hppextensions):
+                cpp = None
+                if self.getCppForHpp is not None:
+                    cpp = self.getCppForHpp(filename) #paths returned here should be consistent with grok, rather than with the codebase
+                elif self.grokscraper is not None:
+                    cpp = self.getCppForHppWithGrok(filename, table)
+                if cpp is None:
+                    raise RuntimeError("Could not find a C++ source file including the header %s!!!!!" % filename)
+                if cpp in table:
+                    table[cpp].add(filename)
+                else:
+                    table[cpp] = set([filename])
+
+    def getCppForHppWithGrok(self, hppfile, table):
+        filename = hppfile.rsplit('/', 1)[1]
+        hpptable = self.grokscraper.getOcurrences("include "+filename)
+        #first check if the file is already in the table
+        for grokfilepath in hpptable:
+            if grokfilepath in table:
+                return grokfilepath
+        #this is a prertty dumbp brute-force approach, it might be far better to avoid greedy strategies and compute a minimal set of cpps for all hpps with ocurrences; however that might be inefficient for codebases with massively nested sets of header files
+        for grokfilepath in hpptable:
+            if grokfilepath.endswith(self.cppextensions):
+                return grokfilepath
+        for grokfilepath in hpptable:
+            if grokfilepath.endswith(self.hppextensions):
+                ret = self.getCppForHppWithGrok(grokfilepath)
+                if ret is not None:
+                    return ret
+        return None
+
+    def doCPPFile(self, term, value, filepath):
+        commandline = self.template(term, value, filepath)+self.compiler_args_base+self.compiler_args(filepath)
+        if self.verbose:
+            print 'ON %s EXECUTE %s' % (self.exedir, ' '.join(commandline))
+        if not self.simulate:
+            self.context.doCommand(commandline, cwd=self.exedir)
+
+    def doXMLFile(self, term, filepath):
+        if self.verbose:
+            print 'ON %s REMOVE REFERNCES TO %s' % (filepath, term)
+        if not self.simulate:
+            if self.context.prepareStep():
+                root = etree.parse(filepath)
+                res = root.xpath(self.xml_xpath % term)
+                if len(res)!=1:
+                    print "Error locating config value <%s> in XML file <%s>!!!!!" % (term, filepath)
+                else:
+                    toremove = res[0]
+                    toremove.getparent().remove(toremove)
+                    with open(filepath, 'w') as svp:
+                        svp.write(etree.tostring(root))
+
+    #main function, does the refactoring
     def doFilesFromTable(self, table, term, value):
-        print "PROCESSING FILES FROM TERMS FOUND WITH OPENGROK\n"
+        if self.verbose:
+            print "PROCESSING FILES FROM TERMS FOUND WITH OPENGROK\n"
         for grokfilepath in sorted(table.keys()):
             lines = list(table[grokfilepath])
             lines.sort()
             filepath = self.translatepath(grokfilepath)
-            savepath = self.translatesavepath(grokfilepath)
             if grokfilepath.endswith(self.cppextensions):
-                print "  TERM <%s> TO BE REFACTORED IN CPP FILE <%s> in line(s) %s" % (term, filepath, lines)
-                self.doCPPFile(term, value, filepath, savepath)
+                if self.verbose:
+                    print "  TERM <%s> TO BE REFACTORED IN CPP FILE <%s> in line(s) %s" % (term, filepath, lines)
+                self.doCPPFile(term, value, filepath)
+            if grokfilepath.endswith(self.hppextensions):
+                if self.verbose:
+                    print "  TERM <%s> FOUND IN HEADER FILE <%s> in line(s) %s (refactored as part of a cpp file)" % (term, filepath, lines)
             elif self.isxmlfile(filepath):
-                print "  TERM <%s> TO BE REFACTORED IN XML FILE <%s> in line(s) %s" % (term, filepath, lines)
-                self.doXMLFile(term, filepath, savepath)
+                if self.verbose:
+                    print "  TERM <%s> TO BE REFACTORED IN XML FILE <%s> in line(s) %s" % (term, filepath, lines)
+                self.doXMLFile(term, filepath)
 
-    def doFilesFromGrok(self, grokscraper, term, value, printRevs=True):
+    #an example of how a high-level funtion to use GrokScraper and ExternalRefactor might look like
+    def doFilesFromGrok(self, term, value, printRevs=True):
         table = grokscraper.getOcurrences(term)
-        doFilesFromTable(table, term, value)
+        self.addCppFilesForHppFiles(table)
+        self.context.startStep()
+        self.doFilesFromTable(table, term, value)
+        self.context.endStep()
         if printRevs:
             print ""
-            revisions = grokscraper.getRevisions(table)
-            gs.printRevisions(revisions)
+            revisions = self.grokscraper.getRevisions(table)
+            self.grokscraper.printRevisions(revisions)
 
 #########################################################
 
 if __name__=='__main__':
     
     translatepath = lambda x: os.path.join('examples', x)
-    external = ExternalRefactor(translatepath=translatepath, translatesavepath=translatepath, compiler_args=lambda x: ["-I./examples/include"], xml_xpath="/config/value[@name='%s']", simulate=False)
+    context = ExecuteContext(verbose=True)
+    external = ExternalRefactor(context, translatepath=translatepath, compiler_args=lambda x: ["-I./examples/include"], xml_xpath="/config/value[@name='%s']", simulate=False, verbose=True)
     table = {'test.cpp':[], 'config.xml':[]}
     external.doFilesFromTable(table, "UseSpanishLanguage", "true") 
