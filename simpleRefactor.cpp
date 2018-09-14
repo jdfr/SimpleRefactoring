7655f1a48506e6e138ebc5e6aefd787342c75f1c
josedavid <josedavid@nospam.com>
2018-09-14 17:20:30 +0200

    some refactoring to facilitate further changes

--- simpleRefactor.cpp
+++ simpleRefactor.cpp
@@ -26,6 +26,8 @@ static llvm::cl::opt<std::string> TermName("term", llvm::cl::cat(CustomOptions),
 static llvm::cl::opt<bool> TermValue("value", llvm::cl::cat(CustomOptions), llvm::cl::desc("config option value"), llvm::cl::value_desc("true/false")); 
 static llvm::cl::opt<bool> Overwrite("overwrite", llvm::cl::cat(CustomOptions), llvm::cl::desc("overwrite source files"), llvm::cl::value_desc("true/false")); 
 
+#define FUNCTION_NAMES "configOption", "configVariable", "config"
+
 //this function was lifted wholesale from clang
 static inline bool isWhitespaceExceptNL(unsigned char c) {
     switch (c) {
@@ -44,8 +46,8 @@ public:
 };
 
 class RefactorIfStmt {
-    Rewriter &Rewrite;
-    SourceManager &SourceMgr;
+    Rewriter *Rewrite;
+    SourceManager *SourceMgr;
     const SrcMgr::ContentCache * Content;
     StringRef fileBuffer;
     FileID FID;
@@ -53,22 +55,22 @@ class RefactorIfStmt {
     bool computed;
     
     void recompute(SourceLocation loc) {
-        FileID f(SourceMgr.getFileID(loc));
+        FileID f(SourceMgr->getFileID(loc));
         if (!computed || f!=FID) {
             computed = true;
             FID = f;
-            fileEntry = SourceMgr.getFileEntryForID(FID);
-            Content = SourceMgr.getSLocEntry(FID).getFile().getContentCache();
-            fileBuffer = SourceMgr.getBufferData(FID);
+            fileEntry = SourceMgr->getFileEntryForID(FID);
+            Content = SourceMgr->getSLocEntry(FID).getFile().getContentCache();
+            fileBuffer = SourceMgr->getBufferData(FID);
         }
     }
     
     //alternative implementation of SourceManager::getComposedLoc() (cannot backport because the original uses private methods), probably more expensive
     SourceLocation getComposedLoc(FileID FID, unsigned Offset) const {
-        //return SourceMgr.getComposedLoc(FID, Offset);
-        unsigned line = SourceMgr.getLineNumber(FID, Offset);
-        unsigned column = SourceMgr.getColumnNumber(FID, Offset);
-        return SourceMgr.translateFileLineCol(fileEntry, line, column);
+        //return SourceMgr->getComposedLoc(FID, Offset);
+        unsigned line = SourceMgr->getLineNumber(FID, Offset);
+        unsigned column = SourceMgr->getColumnNumber(FID, Offset);
+        return SourceMgr->translateFileLineCol(fileEntry, line, column);
     }
   
     IndentRange getIndentRange(unsigned lineNumber) {
@@ -83,7 +85,7 @@ class RefactorIfStmt {
     }
 
     unsigned getLine(SourceLocation loc) {
-        return SourceMgr.getLineNumber(FID, SourceMgr.getFileOffset(loc));
+        return SourceMgr->getLineNumber(FID, SourceMgr->getFileOffset(loc));
     }
     
     IndentRange reindentBranch(const IfStmt *IfS, const Stmt *branch) {
@@ -99,14 +101,16 @@ class RefactorIfStmt {
             int lenIndent = indentRange.size;
             int lenToRemove = lenIndent-lenIfIndent;
             if (lenToRemove>0) {
-                Rewrite.RemoveText(getComposedLoc(FID, indentRange.end()-lenToRemove), lenToRemove);
+                Rewrite->RemoveText(getComposedLoc(FID, indentRange.end()-lenToRemove), lenToRemove);
             }
         }
         return ifIndentRange;
     }
     
 public:
-    RefactorIfStmt(Rewriter &R) : Rewrite(R), SourceMgr(R.getSourceMgr()), computed(false) {}
+    RefactorIfStmt() : computed(false) {}
+
+    void setRewriter(Rewriter *R) { Rewrite = R; SourceMgr = &R->getSourceMgr(); computed = false; }
     
     void simpleRefactoIfStmt(const IfStmt *IfS) {
         //const Expr *cond = IfS->getCond();
@@ -119,15 +123,15 @@ public:
             //the if keyword's indent range might be handy later
             IndentRange ifIndentRange = reindentBranch(IfS, branch);
             if (TermValue.getValue()) {
-                Rewrite.RemoveText(SourceRange(IfS->getIfLoc(), Then->getLocStart().getLocWithOffset(-1))); 
-                Rewrite.RemoveText(SourceRange(Then->getLocEnd().getLocWithOffset(1), IfS->getLocEnd()));
+                Rewrite->RemoveText(SourceRange(IfS->getIfLoc(), Then->getLocStart().getLocWithOffset(-1)));
+                Rewrite->RemoveText(SourceRange(Then->getLocEnd().getLocWithOffset(1), IfS->getLocEnd()));
             } else {
-                Rewrite.RemoveText(SourceRange(IfS->getIfLoc(), Else->getLocStart().getLocWithOffset(-1)));
+                Rewrite->RemoveText(SourceRange(IfS->getIfLoc(), Else->getLocStart().getLocWithOffset(-1)));
             }
             if (compound) {
                 const CompoundStmt &branchC = cast<CompoundStmt>(branchR);
-                Rewrite.RemoveText(branchC.getLBracLoc(), 1);
-                Rewrite.RemoveText(branchC.getRBracLoc(), 1);
+                Rewrite->RemoveText(branchC.getLBracLoc(), 1);
+                Rewrite->RemoveText(branchC.getRBracLoc(), 1);
             } else if (ifIndentRange.size>0){
                 /* At least in branches of conditional statements, clang treats the whitespace before simple
                 * statements as part of them, while compound statements just ignore whitespace. The unpleasant
@@ -135,32 +139,31 @@ public:
                 * working purely from the AST, as simple statements have one indelible whitespace character.
                 * This is a hack to get decent indentation for simple statements, most of the time, provided
                 * there are no inconvenient tabs within the whitespace. */
-                Rewrite.RemoveText(getComposedLoc(FID, ifIndentRange.end()-1), 1);
+                Rewrite->RemoveText(getComposedLoc(FID, ifIndentRange.end()-1), 1);
             }
         } else {
-            Rewrite.RemoveText(IfS->getSourceRange());  
+            Rewrite->RemoveText(IfS->getSourceRange());
         }
     }
 
 };
 
 
-class IfStmtHandler : public MatchFinder::MatchCallback {
+class Handler : public MatchFinder::MatchCallback {
 public:
-  IfStmtHandler(Rewriter &_Rewrite, std::string _literal) : Rewrite(_Rewrite), reindentTool(_Rewrite), literal(_literal) {}
+  Handler(RefactorIfStmt *r, std::string _literal) : reindentTool(r), literal(_literal) {}
 
   virtual void run(const MatchFinder::MatchResult &Result) {
         // The matched 'if' statement was bound to 'ifStmt'.
         const IfStmt *IfS = Result.Nodes.getNodeAs<clang::IfStmt>("ifStmt");
         const StringLiteral *lit = Result.Nodes.getNodeAs<clang::StringLiteral>("strLiteral");
         if (IfS && lit && lit->getString().str() == literal) {
-            reindentTool.simpleRefactoIfStmt(IfS);
+            reindentTool->simpleRefactoIfStmt(IfS);
         }
   }
 
 private:
-    Rewriter &Rewrite;
-    RefactorIfStmt reindentTool;
+    RefactorIfStmt *reindentTool;
     std::string literal;
 };
 
@@ -169,14 +172,14 @@ private:
 // the AST.
 class MyASTConsumer : public ASTConsumer {
 public:
-    MyASTConsumer(Rewriter &R) : HandlerForIf(R, TermName.getValue()) {
+    MyASTConsumer(RefactorIfStmt *R) : handler(R, TermName.getValue()) {
         // Add a simple matcher for finding 'if' statements.
 
         Matcher.addMatcher(
             ifStmt(
                 hasCondition(has(ignoringParenImpCasts(
                     callExpr(
-                        callee(functionDecl(hasAnyName("configOption", "configVariable", "config"))),
+                        callee(functionDecl(hasAnyName(FUNCTION_NAMES))),
                         hasArgument(0, allOf(
                                             hasDescendant(stringLiteral().bind("strLiteral")),
                                             unless(anyOf(
@@ -188,7 +191,7 @@ public:
                      )//.bind("callToConfigFunction")
                 )))
             ).bind("ifStmt"), 
-            &HandlerForIf
+            &handler
         );
     }
 
@@ -198,7 +201,7 @@ public:
   }
 
 private:
-  IfStmtHandler HandlerForIf;
+  Handler handler;
   MatchFinder Matcher;
 };
 
@@ -222,11 +225,13 @@ public:
 
   std::unique_ptr<ASTConsumer> CreateASTConsumer(CompilerInstance &CI, StringRef file) override {
     TheRewriter.setSourceMgr(CI.getSourceManager(), CI.getLangOpts());
-    return llvm::make_unique<MyASTConsumer>(TheRewriter);
+    reindentTool.setRewriter(&TheRewriter);
+    return llvm::make_unique<MyASTConsumer>(&reindentTool);
   }
 
 private:
   Rewriter TheRewriter;
+  RefactorIfStmt reindentTool;
 };
 
 int main(int argc, const char **argv) {
